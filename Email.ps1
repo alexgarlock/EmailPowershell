be3c3dce103688d3265d62dbcbe89fab20e7e4c4
Alex Garlock <alexgarlock@me.com>
2020-08-28 21:07:25 -0400

    Add files via upload

---
+++ Email.ps1
@@ -0,0 +1,12 @@
+﻿##This is simple code to send an email from setting up an event in windows. 
+
+$secret = ConvertTo-SecureString "password" -AsPlainText -Force
+$techCreds = New-Object System.Management.Automation.PSCredential("username",$secret)
+
+$SMTPServer= "smtp.com"
+$From = "alex@alex.com"
+$To = "alex.garlock@yahoo.com","alexgarlock@me.com"
+$Subject = "Test email"
+$Body = "This is a test to see if we can send an email"
+
+Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer $SMTPServer  -Credential $techCreds 
